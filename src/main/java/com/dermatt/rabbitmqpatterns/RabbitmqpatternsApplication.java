8947ff45fa8774e4c2438c96d4d8343fb05d8c57
dermatt02 <dermatt02@gmail.com>
2026-06-03 12:16:07 -0500

    Initial project setup: Spring Boot 4.0.6 + Apache Camel + RabbitMQ

---
+++ src/main/java/com/dermatt/rabbitmqpatterns/RabbitmqpatternsApplication.java
@@ -0,0 +1,13 @@
+package com.dermatt.rabbitmqpatterns;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RabbitmqpatternsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RabbitmqpatternsApplication.class, args);
+	}
+
+}
