dc8c20a8d34e8d22a3523832e9e43910fdbc68a5
Galo Guevara <galo.guevara.torres@udla.edu.ec>
2026-06-07 22:45:10 -0500

    fix: add atributos faltantes del request

--- src/main/java/com/dermatt/rabbitmqpatterns/model/OrderRequest.java
+++ src/main/java/com/dermatt/rabbitmqpatterns/model/OrderRequest.java
@@ -1,13 +1,22 @@
 package com.dermatt.rabbitmqpatterns.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class OrderRequest {
 
+    private String messageId;
+    private String messageType;
     private String orderId;
     private String customerId;
     private double total;
 
     public OrderRequest() {}
 
+    public String getMessageId() { return messageId; }
+    public void setMessageId(String messageId) { this.messageId = messageId; }
+    public String getMessageType() { return messageType; }
+    public void setMessageType(String messageType) { this.messageType = messageType; }
     public String getOrderId() { return orderId; }
     public void setOrderId(String orderId) { this.orderId = orderId; }
     public String getCustomerId() { return customerId; }
